5e7785313c37649473db30786e063e40add63175
M.S.T.O.P. <mstop4@gmail.com>
2025-08-10 18:33:58 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,31 @@ import { useState } from 'react';
 import { useCalculator } from './store/calculatorStore';
 
 function App() {
-  const [display, setDisplay] = useState('_');
+  const [commandDisplay, setCommandDisplay] = useState('_');
+  const [resultDisplay, setResultDisplay] = useState('_');
   const addCommand = useCalculator((state) => state.addCommand);
 
   const handleAddClick = () => {
     addCommand(8);
+    const { commands } = useCalculator.getState();
+    setCommandDisplay(
+      commands.reduce((total, command) => `${total}${command.toString()} `, '')
+    );
   };
 
   const handleCalculateClick = () => {
     const { commands } = useCalculator.getState();
     console.log(commands);
     const result = commands.reduce((total, command) => total + command, 0);
-    setDisplay(result.toString());
+    setResultDisplay(result.toString());
   };
 
   return (
     <>
       <button onClick={handleAddClick}>Add</button>
       <button onClick={handleCalculateClick}>Calculate</button>
-      <p>{display}</p>
+      <p>{commandDisplay}</p>
+      <p>{resultDisplay}</p>
     </>
   );
 }
